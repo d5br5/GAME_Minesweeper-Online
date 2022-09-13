b05d99cddfafc0fba33a907aa40fa54d275bb0f9
Doh Kim <ehgud456456@naver.com>
2022-09-13 16:33:49 +0900

    Squashed commit of the following:
    commit 8f639dc41f96dd27698465ab945ee44c57fd45a6
    Author: Doh Kim <ehgud456456@naver.com>
    Date:   Tue Sep 13 16:31:25 2022 +0900
    
        [Docs] commit, branch rule
    
    commit 469e1c93cf970c85fab820dcb83afba8700cdc4a
    Author: Doh Kim <ehgud456456@naver.com>
    Date:   Tue Sep 13 16:31:01 2022 +0900
    
        [Init] Emotion

---
+++ pages/home.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+const SHeader = styled.div`
+	color: red;
+`;
+
+const Home = () => {
+	return (
+		<div>
+			<SHeader>test</SHeader>
+		</div>
+	);
+};
+
+export default Home;

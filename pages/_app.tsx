2bdaf8e9f2f7998411aa45ffe5e0468d05be09f8
Doh Kim <ehgud456456@naver.com>
2022-09-15 18:33:47 +0900

    [Init] prisma, planetscale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@components/layout";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
+	const router = useRouter();
+	console.log(router);
+	return router.pathname === "/" ? (
+		<Component {...pageProps} />
+	) : (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
 	);
 }
 

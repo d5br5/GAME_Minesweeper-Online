fcc87235b3c05019617ab54d31fff3ffd40a4e67
Doh Kim <ehgud456456@naver.com>
2022-09-26 22:05:37 +0900

    [Init] SWR setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { SWRConfig } from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
-			<Component {...pageProps} />
+			<SWRConfig value={{ fetcher }}>
+				<Component {...pageProps} />
+			</SWRConfig>
 		</RecoilRoot>
 	);
 }
